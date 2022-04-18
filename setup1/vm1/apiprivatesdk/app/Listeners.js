8e7a9ee14f78ec7885f09313338b779b55956a2d
Khushal Kunjir <99632414+khushalSnapper@users.noreply.github.com>
2022-04-18 15:25:05 +0530

    Added VM1 related files

---
+++ setup1/vm1/apiprivatesdk/app/Listeners.js
@@ -0,0 +1,239 @@
+const { BlockListener } = require("fabric-network");
+const { FAILSAFE_SCHEMA } = require("js-yaml");
+let request = require('request')
+const constants = require('../config/constants.json');
+var temp = null;
+function showTransactionData(transactionData) {
+    console.log(JSON.stringify(transactionData))
+    const creator = transactionData.actions[0].header.creator;
+    console.log(`    - submitted by: ${creator.mspid}-${creator.id_bytes.toString('hex')}`);
+    for (const endorsement of transactionData.actions[0].payload.action.endorsements) {
+        console.log(`    - endorsed by: ${endorsement.endorser.mspid}-${endorsement.endorser.id_bytes.toString('hex')}`);
+    }
+    const chaincode = transactionData.actions[0].payload.chaincode_proposal_payload.input.chaincode_spec;
+    console.log(`    - chaincode:${chaincode.chaincode_id.name}`);
+    console.log(`    - function:${chaincode.input.args[0].toString()}`);
+    for (let x = 1; x < chaincode.input.args.length; x++) {
+        console.log(`    - arg:${chaincode.input.args[x].toString()}`);
+    }
+}
+
+// contractListener = async (event) => {
+//     console.log("==========================================")
+//     console.log(event)
+//     // The payload of the chaincode event is the value place there by the
+//     // chaincode. Notice it is a byte data and the application will have
+//     // to know how to deserialize.
+//     // In this case we know that the chaincode will always place the asset
+//     // being worked with as the payload for all events produced.
+//     const asset = JSON.parse(event.payload.toString());
+//     console.log(`<-- Contract Event Received: ${event.eventName} - ${JSON.stringify(asset)}`);
+//     // show the information available with the event
+//     console.log(`*** Event: ${event.eventName}:${asset.ID}`);
+//     // notice how we have access to the transaction information that produced this chaincode event
+//     const eventTransaction = event.getTransactionEvent();
+//     console.log(`*** transaction: ${eventTransaction.transactionId} status:${eventTransaction.status}`);
+//     showTransactionData(eventTransaction.transactionData);
+//     // notice how we have access to the full block that contains this transaction
+//     const eventBlock = eventTransaction.getBlockEvent();
+//     console.log(`*** block: ${eventBlock.blockNumber.toString()}`);
+// };
+
+
+// blockListener = async (event) => {
+
+//     console.log("--------------------------------------------------------------")
+//     console.log(`<-- Block Event Received - block number: ${event.blockNumber.toString()}`);
+
+//     const transEvents = event.getTransactionEvents();
+//     for (const transEvent of transEvents) {
+//         console.log(`*** transaction event: ${transEvent.transactionId}`);
+//         // if (transEvent.privateData) {
+//         //     for (const namespace of transEvent.privateData.ns_pvt_rwset) {
+//         //         console.log(`    - private data: ${namespace.namespace}`);
+//         //         for (const collection of namespace.collection_pvt_rwset) {
+//         //             console.log(`     - collection: ${collection.collection_name}`);
+//         //             if (collection.rwset.reads) {
+//         //                 for (const read of collection.rwset.reads) {
+//         //                     console.log(`       - read set - ${BLUE}key:${RESET} ${read.key}  ${BLUE}value:${read.value.toString()}`);
+//         //                 }
+//         //             }
+//         //             if (collection.rwset.writes) {
+//         //                 for (const write of collection.rwset.writes) {
+//         //                     console.log(`      - write set - ${BLUE}key:${RESET}${write.key} ${BLUE}is_delete:${RESET}${write.is_delete} ${BLUE}value:${RESET}${write.value.toString()}`);
+//         //                 }
+//         //             }
+//         //         }
+//         //     }
+//         // }
+//         if (transEvent.transactionData) {
+//             showTransactionData(transEvent.transactionData);
+//         }
+//     }
+// };
+
+
+const blockListener = async (event) => {
+    const bdata = event.blockData.data.data;
+
+    console.log(bdata);
+
+    const transEvents = event.getTransactionEvents();
+    //console.log(transEvents[0].transactionData.actions[0].payload.header.channel_header.timestamp);
+    console.log(transEvents[0].transactionData.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].collection_hashed_rwset)
+    console.log("transEvents length ", transEvents.length)
+    for (const transEvent of transEvents) {
+        console.log(`*** transaction event: ${transEvent.transactionId}`);
+        if (transEvent.privateData) {
+            for (const namespace of transEvent.privateData.ns_pvt_rwset) {
+                console.log(`    - private data: ${namespace.namespace}`);
+                for (const collection of namespace.collection_pvt_rwset) {
+                    console.log(`     - collection: ${collection.collection_name}`);
+                    if (collection.rwset.reads) {
+                        for (const read of collection.rwset.reads) {
+                            console.log(read)
+                            // console.log(`       - read set - ${BLUE}key:${RESET} ${read.key}  ${BLUE}value:${read.value.toString()}`);
+                        }
+                    }
+                    if (collection.rwset.writes) {
+                        console.log("collection.rwset.writes", collection.rwset.writes.length)
+                        for (const write of collection.rwset.writes) {
+                            a = write.value.toString();
+                            b = JSON.parse(a);
+                            console.log("##############", b)
+                            let privateData = JSON.parse(b.MongoDBJson.replace(/\'/g, '"'));
+                            console.log("*******************", privateData.orgName);
+                            let finalResult;
+                            // const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+                            // if(finalResult === undefined){
+                            console.log(typeof event.blockNumber.low)
+                            //    var temp = event.blockNumber.low;
+                            let validEvent = await checkBlockNumber(event.blockNumber.low)
+                            
+                             console.log("validEvent",validEvent)
+                            if (validEvent) {
+                                console.log("event is valid")
+                                if (privateData.orgName != constants.orgName) {
+                                    let returnResult = request.post({
+                                        url: 'http://20.221.240.130:3000/api/v1/blockchainEvents/blockchain_events',
+                                        json: true,
+                                        traditional: true,
+                                        body: b
+                                    }, (err, result) => {
+                                        if (!err) {
+                                            console.log("temp", temp)
+                                            console.log("((((((", result.body)
+                                            // temp++;
+                                            return result.body
+                                        }
+                                        // console.log("Result Body========", result.body)
+                                    })
+                                } else {
+                                    console.log("Skip the process of updating mongodb because organization is same",constants.orgName)
+                                }
+                            } else {
+                                console.log("event is not valid")
+                            }
+                            // await sleep(5000);
+                            // console.log(returnResult);
+                            // console.log(returnResult.Request);
+                            // console.log(returnResult.Request.body);
+                            // finalResult=returnResult.Request.new;
+                            // if(JSON.stringify(b) === JSON.stringify(finalResult)){
+                            //     console.log("******************************",returnResult.Request.new )
+                            // } else {
+                            //     console.log("#####################")
+                            // }
+
+                            // } else {
+                            //   console.log(temp)
+                            // }
+                            console.log("new", b);
+
+                            // console.log(`      - write set - ${BLUE}key:${RESET}${write.key} ${BLUE}is_delete:${RESET}${write.is_delete} ${BLUE}value:${RESET}${write.value.toString()}`);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    console.log("Block Event Received...")
+    console.log("Block Number: ", event.blockNumber.low)
+    console.log("Total Transactions: ", event.getTransactionEvents().length)
+
+    console.log("Tx Type: ", event.blockData.data.data[0].payload.header.channel_header.type)
+    console.log("Tx Timestamp: ", event.blockData.data.data[0].payload.header.channel_header.timestamp)
+
+    console.log("Block Data[0].actions[0].ns_rwset[0]: ", event.blockData.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[0].namespace)
+    console.log("Block Data[0].actions[0].ns_rwset[1]: ", event.blockData.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].namespace)
+    console.log("Block Data[0].actions[0].ns_rwset[1].writeset[0].KEY: ", event.blockData.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].rwset.writes[0].key)
+    // const event_data=event.blockData.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].rwset.writes[0].value.toString();
+    console.log("Block Data[0].actions[0].ns_rwset[1].writeset[0].VALUE: ", event.blockData.data.data[0].payload.data.actions[0].payload.action.proposal_response_payload.extension.results.ns_rwset[1].rwset.writes[0].value.toString())
+    // 
+    // request.post({
+    //     url: 'http://localhost:3000/api/v1/user/asset_event',
+    //     json: true,
+    //     traditional: true,
+    //     body: transEvents,
+    //     headers: {
+
+    //     }
+    // },(err, result)=>{
+    //    // console.log("Result Body========", result.body)
+    // })
+}
+var previousText = null;
+function checkBlockNumber(number) {
+  var currentText = number;
+  if (previousText == currentText) {
+      console.log("!!!!!!!!!!!",previousText,currentText)
+      previousText = currentText;
+    return false;
+  } else {
+    console.log("!!!!!!!!!!!",previousText,currentText)
+    previousText = currentText;
+     return true;
+  }
+}
+
+// const checkBlockNumber = async function (blockNumber) {
+//     console.log("temp blockNumber", temp, blockNumber)
+//     if (temp !== blockNumber) {
+//         return true;
+//     } else if (temp === blockNumber) {
+//         return false;
+//     }
+//     temp = blockNumber;
+// }
+
+const contractListener = async (event) => {
+    console.log("contract Event Received...")
+
+    //console.log("Chaincode ID: ", chaincodeID)
+    console.log("Event Name: ", event.eventName)
+    console.log("Payload: ", event.payload.toString())
+}
+
+const commitListener = async (error, event) => {
+    console.log("Commit Listener Called...")
+    if (error) {
+        console.log("Error Occured in commit Listener: ", error)
+    } else {
+        console.log("Transaction Validity: ", event.peer.name)
+        console.log("Contract Events Count: ", event.getContractEvents().length)
+        const contractEvents = event.getContractEvents()
+        console.log(contractEvents)
+        console.log("Event Name: ", contractEvents[0].eventName)
+        const txEvents = contractEvents.getTransactionEvents()
+        console.log(txEvents)
+        console.log(txEvents.isValid)
+    }
+
+}
+
+
+module.exports = {
+    contractListener,
+    blockListener,
+    commitListener
+}
---
+++ setup1/vm1/apiprivatesdk/app/invoke.js
@@ -0,0 +1,264 @@
+const { Gateway, Wallets, TxEventHandler, GatewayOptions, DefaultEventHandlerStrategies, TxEventHandlerFactory, DefaultCheckpointers } = require('fabric-network');
+const fs = require('fs');
+const EventStrategies = require('fabric-network/lib/impl/event/defaulteventhandlerstrategies');
+const path = require("path")
+const log4js = require('log4js');
+const logger = log4js.getLogger('TracechainNetwork');
+const util = require('util')
+
+const helper = require('./helper');
+const { blockListener, contractListener, commitListener } = require('./Listeners');
+const { resolve } = require('path');
+const { fullList } = require('npm');
+
+const submitTransactionWithPrivateData = async (contract, fcn, args1, args2, transientData) => {
+    try {
+        let transientJSON = JSON.parse(transientData);
+        let key = Object.keys(transientJSON)[0];
+        let result;
+        const transientDataBuffer = {};
+        transientDataBuffer[key] = Buffer.from(JSON.stringify((transientJSON.asset_properties)));
+        for (let i = 0; i < 10; i++) {
+            try {
+                result = await contract.createTransaction(fcn)
+                    .setTransient(transientDataBuffer)
+                    .submit(args1, args2)
+                console.log("Result ((((((((((((((((((((((((((", result)
+                break;
+            } catch (error) {
+                console.log(`Creation of transaction failed attempting again [Attempt: ${i}] `)
+                i++;
+            }
+
+        }
+        if (result !== undefined) {
+            return result;
+        } else {
+            console.log(`Invoke Transaction Getting error: ${error}`);
+            const response_payload = {
+                result: null,
+                error: true
+            };
+            return response_payload;
+        }
+    } catch (error) {
+        console.log(`Invoke Transaction Getting error: ${error}`);
+        const response_payload = {
+            result: null,
+            error: true,
+            errorData: error.message
+        };
+        return response_payload;
+    }
+}
+
+const submitTransactionWithUpdatePrivateData = async (contract, fcn, args0, args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13, args14, args15, args16, args17, args18, args19, args20, args21, args22, args23, transientData) => {
+    try {
+        let transientJSON = JSON.parse(transientData);
+        let key = Object.keys(transientJSON)[0];
+        let result;
+        const transientDataBuffer = {};
+        transientDataBuffer[key] = Buffer.from(JSON.stringify((transientJSON.asset_properties)));
+        for (let i = 0; i < 10; i++) {
+            try {
+                result = await contract.createTransaction(fcn)
+                    .setTransient(transientDataBuffer)
+                    .submit(args0, args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13, args14, args15, args16, args17, args18, args19, args20, args21, args22, args23)
+                break;
+            } catch (error) {
+                console.log(`Creation of transaction failed attempting again [Attempt: ${i}] `)
+                i++;
+            }
+
+        }
+        if (result !== undefined) {
+            return result;
+        } else {
+            console.log(`Invoke Transaction Getting error: ${error}`);
+            const response_payload = {
+                result: null,
+                error: true
+            };
+            return response_payload;
+        }
+    } catch (error) {
+        console.log(`Invoke Transaction Getting error: ${error}`);
+        const response_payload = {
+            result: null,
+            error: true,
+            errorData: error.message
+        };
+        return response_payload;
+    }
+}
+
+
+const submitTransactionWithDeletePrivateData = async (contract, fcn, args0, args1, transientData) => {
+    try {
+        let transientJSON = JSON.parse(transientData);
+        let key = Object.keys(transientJSON)[0];
+        let result;
+        const transientDataBuffer = {};
+        transientDataBuffer[key] = Buffer.from(JSON.stringify((transientJSON.asset_properties)));
+        for (let i = 0; i < 10; i++) {
+            try {
+                result = await contract.createTransaction(fcn)
+                    .setTransient(transientDataBuffer)
+                    .submit(args0, args1)
+                break;
+            } catch (error) {
+                console.log(`Creation of transaction failed attempting again [Attempt: ${i}] `)
+                i++;
+            }
+
+        }
+        if (result !== undefined) {
+            return result;
+        } else {
+            console.log(`Invoke Transaction Getting error: ${error}`);
+            const response_payload = {
+                result: null,
+                error: true
+            };
+            return response_payload;
+        }
+    } catch (error) {
+        console.log(`Invoke Transaction Getting error: ${error}`);
+        const response_payload = {
+            result: null,
+            error: true,
+            errorData: error.message
+        };
+        return response_payload;
+    }
+}
+
+const invokeTransaction = async (channelName, chaincodeName, fcn, args, username, org_name, transientData) => {
+    try {
+        const ccp = await helper.getCCP(org_name);
+
+        const walletPath = await helper.getWalletPath(org_name);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        let identity = await wallet.get(username);
+        if (!identity) {
+            console.log(`An identity for the user ${username} does not exist in the wallet, so registering user`);
+            await helper.getRegisteredUser(username, org_name, true)
+            identity = await wallet.get(username);
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+
+        const connectOptions = {
+            wallet, identity: username, discovery: { enabled: true, asLocalhost: false },
+            // eventHandlerOptions: EventStrategies.NONE
+            eventHandlerOptions: {
+                commitTimeout: 400000,
+                strategy: DefaultEventHandlerStrategies.NETWORK_SCOPE_ALLFORTX
+            }
+        }
+
+        const gateway = new Gateway();
+        await gateway.connect(ccp, connectOptions);
+        // await gateway.disconnect();
+
+
+        const network = await gateway.getNetwork(channelName);
+
+        const contract = network.getContract(chaincodeName);
+
+        let result;
+        // console.log(contract.sendInstantiateProposal)
+
+        switch (fcn) {
+            case "InsertAssetRecords":
+                console.log("====== Executing InsertAssetRecords ======")
+                //result = await contract.submitTransaction(fcn, args[0]);
+                if (parseInt(args[1]) === 0) {
+                    console.log("inside if")
+                    // Submit transaction with private data 
+                    result = await submitTransactionWithPrivateData(contract, fcn, args[0], args[1], transientData)
+                } else {
+                    console.log("inside else Result")
+                    //submit tranascation without private data 
+                    result = await contract.submitTransaction(fcn, args[0], args[1])
+                    console.log("Result", result)
+                }
+                console.log("#######################", JSON.stringify(result))
+                console.log(`Output: InsertAssetRecords= ${result.toString()}`)
+                result = { txid: result.toString() }
+                break;
+            case "InsertOrderRecords":
+                console.log("====== Executing InsertOrderRecords ======")
+                //result = await contract.submitTransaction(fcn, args[0]);
+                if (parseInt(args[1]) === 0) {
+                    result = await submitTransactionWithPrivateData(contract, fcn, args[0], args[1], transientData)
+                } else {
+                    result = await contract.submitTransaction(fcn, args[0], args[1])
+                }
+                console.log(`Output: InsertOrderRecords= ${result.toString()}`)
+                result = { txid: result.toString() }
+                break;
+            case "UpdateAsset":
+                console.log("====== Executing UpdateAsset ======")
+                if (parseInt(args[23]) === 0) {
+                    result = await submitTransactionWithUpdatePrivateData(contract, fcn, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17], args[18], args[19], args[20], args[21], args[22], args[23], transientData);
+                } else {
+                    result = await contract.submitTransaction(fcn, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17], args[18], args[19], args[20], args[21], args[22]);
+                }
+                console.log(`Output: UpdateAsset= ${result.toString()}`)
+                result = { txid: result.toString() }
+                break;
+            case "DeleteAssetByTransactionId":
+                console.log("====== Executing DeleteAssetByTransactionId ======")
+                if (parseInt(args[1]) === 0) {
+                    result = await submitTransactionWithDeletePrivateData(contract, fcn, args[0], args[1], transientData);
+                } else {
+                    result = await contract.submitTransaction(fcn, args[0], args[1]);
+                }
+                console.log(`Output: DeleteAssetByTransactionId= ${result.toString()}`)
+                result = { txid: result.toString() }
+                break;
+            case "GetAssetForQuery":
+                console.log("======= Executing GetAssetForQuery =======")
+                result = await contract.submitTransaction(fcn, args[0]);
+                result = { txid: result.toString() }
+                break;
+
+            default:
+                break;
+        }
+
+
+
+        await network.addBlockListener(blockListener, { type: "private" })
+
+
+
+        const response_payload = {
+            result: result,
+            error: false,
+            errorData: null
+        }
+
+        console.log("Invoke Transaction Try Block Response Payload: ", response_payload)
+        return response_payload;
+
+
+    } catch (error) {
+
+        console.log(`Invoke Transaction Getting error: ${error}`)
+        const response_payload = {
+            result: null,
+            error: true,
+            errorData: error.message
+        }
+        console.log("Invoke Transaction Catch Block Response Payload: ", response_payload)
+        return response_payload
+
+    }
+}
+
+exports.invokeTransaction = invokeTransaction;
